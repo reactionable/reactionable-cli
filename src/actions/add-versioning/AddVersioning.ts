1d79ecabe674a8091b26f290aef3e93f8dbd6612
Neilime <escalle.emilien@gmail.com>
2019-11-01 13:57:42 +0100

    feat(add-versioning): rename add-versionning adapter 'Git' to 'Github'
    Move generic git workflow to an abstract class, leave specific github workflow in Github adapter
    
    BREAKING CHANGE: Git adapter do not exists anymore, replaced by Github

--- src/actions/add-versioning/AddVersioning.ts
+++ src/actions/add-versioning/AddVersioning.ts
@@ -3,22 +3,33 @@ import { injectable } from 'inversify';
 import { IRealpathRunnable } from '../IRealpathRunnable';
 import container from './container';
 import { IVersioningAction } from './versionings/IVersioningAction';
+import { success, info } from '../../plugins/Cli';
 
 @injectable()
 export default class AddVersioning implements IRealpathRunnable {
     async run(options) {
-        const answer = await prompt([
-            {
-                name: 'versioning',
-                message: 'Which versioning do you want to add?',
-                type: 'list',
-                choices: container.getAll<IVersioningAction>('Versioning').map(versioning => ({
+        info('Adding Versioning...');
+        const { versioning } = await prompt<{ versioning: IVersioningAction | false }>([{
+            name: 'versioning',
+            message: 'Which versioning do you want to add?',
+            type: 'list',
+            choices: [
+                ...container.getAll<IVersioningAction>('Versioning').map(versioning => ({
                     'name': versioning.getName(),
                     'value': versioning,
                 })),
-            },
-        ]);
+                {
+                    'name': 'None',
+                    'value': false,
+                },
+            ],
+        }]);
 
-        await answer.versioning.run(options);
+        if (!versioning) {
+            return;
+        }
+
+        await versioning.run(options);
+        success('Versioning has been added in "' + options.realpath + '"');
     }
 }
