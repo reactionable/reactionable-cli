65a54b50018bf95415bb5ff39eb96cf8a6f1c7c0
Emilien Escalle <emilien.escalle@escemi.com>
2021-04-09 18:09:51 +0200

    chore(deps): clean and update dependencies

--- src/cli/Cli.ts
+++ src/cli/Cli.ts
@@ -1,35 +1,35 @@
-import { resolve } from 'path';
+import { resolve } from "path";
 
-import { Cli as Clipanion, Command } from 'clipanion';
-import { textSync } from 'figlet';
-import prompts from 'prompts';
+import { Builtins, Cli as Clipanion, Command } from "clipanion";
+import { textSync } from "figlet";
+import prompts from "prompts";
 
-import { NamedAction } from '../actions/NamedAction';
-import container from '../container';
-import { CliService } from '../services/CliService';
-import { ConsoleService } from '../services/ConsoleService';
-import { PackageManagerService } from '../services/package-manager/PackageManagerService';
+import { NamedAction } from "../actions/NamedAction";
+import container from "../container";
+import { CliService } from "../services/CliService";
+import { ConsoleService } from "../services/ConsoleService";
+import { PackageManagerService } from "../services/package-manager/PackageManagerService";
 
 // run [-v,--verbose] [--name ARG]
 class RunCommand extends Command {
+  static paths = [[`run`]];
   //   @Command.Boolean(`-v,--verbose`)
   //   public verbose = false;
 
   //   @Command.String(`--name`)
   //   public name?: string;
 
-  @Command.Path(`run`)
   async execute() {
     try {
       container.get(CliService).initRunStartDate();
 
       const { action } = await prompts([
         {
-          name: 'action',
-          message: 'What do you want to do?',
-          type: 'select',
+          name: "action",
+          message: "What do you want to do?",
+          type: "select",
           choices: [
-            ...container.getAll<NamedAction>('Action').map((action) => ({
+            ...container.getAll<NamedAction>("Action").map((action) => ({
               title: action.getName(),
               value: action,
             })),
@@ -39,8 +39,8 @@ class RunCommand extends Command {
 
       const { projectDir } = await prompts([
         {
-          type: 'text',
-          name: 'projectDir',
+          type: "text",
+          name: "projectDir",
           message: `Where to you you want to ${action.getName().toLowerCase()} (path)?`,
           initial: process.cwd(),
           format: (input) => resolve(input),
@@ -67,21 +67,21 @@ export class Cli {
       binaryVersion: await this.getBinaryVersion(),
       enableColors: true,
     });
-    this.cli.register(Command.Entries.Help);
-    this.cli.register(Command.Entries.Version);
+    this.cli.register(Builtins.HelpCommand);
+    this.cli.register(Builtins.VersionCommand);
     this.cli.register(RunCommand);
   }
 
   protected getBinaryLabel(): string {
-    return `\n${textSync('Reactionable', {
-      font: 'Small Slant',
-      horizontalLayout: 'default',
-      verticalLayout: 'default',
+    return `\n${textSync("Reactionable", {
+      font: "Small Slant",
+      horizontalLayout: "default",
+      verticalLayout: "default",
     })}`;
   }
 
   protected getBinaryVersion(): Promise<string | undefined> {
-    return container.get(PackageManagerService).getPackageVersion(resolve(__dirname, '../..'));
+    return container.get(PackageManagerService).getPackageVersion(resolve(__dirname, "../.."));
   }
 
   async run(): Promise<void> {
