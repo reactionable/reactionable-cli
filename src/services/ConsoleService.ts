364a9483f4ff72bb0725374e85ce8651bcd02ca0
Emilien Escalle <emilien.escalle@escemi.com>
2023-11-10 20:12:00 +0100

    feat: upgrade dependencies

--- src/services/ConsoleService.ts
+++ src/services/ConsoleService.ts
@@ -1,28 +1,30 @@
-import chalk from "chalk";
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
+import { ColorService } from "./ColorService";
 
 @injectable()
 export class ConsoleService {
+  constructor(@inject(ColorService) private readonly colorService: ColorService) {}
+
   info(message: string): void {
-    process.stdout.write(`\n${chalk.yellow("→ ")}${message}\n`);
+    process.stdout.write(`\n${this.colorService.yellow("→ ")}${message}\n`);
   }
 
   success(message: string): void {
-    process.stdout.write(`${chalk.green("✓ ") + message}\n`);
+    process.stdout.write(`${this.colorService.green("✓ ") + message}\n`);
   }
 
   error(error: unknown): void {
     if (typeof error === "string") {
-      process.stderr.write(`\n${chalk.red(`⚠ ${error}`)}\n`);
+      process.stderr.write(`\n${this.colorService.red(`⚠ ${error}`)}\n`);
       return;
     }
     if (error instanceof Error) {
       process.stderr.write(
-        `\n${chalk.red(`⚠ [${error.name}] ${error.message}`)}\n${error.stack}\n`
+        `\n${this.colorService.red(`⚠ [${error.name}] ${error.message}`)}\n${error.stack}\n`
       );
       return;
     }
 
-    process.stderr.write(`\n${chalk.red(`⚠ `)}${JSON.stringify(error)}\n`);
+    process.stderr.write(`\n${this.colorService.red(`⚠ `)}${JSON.stringify(error)}\n`);
   }
 }
